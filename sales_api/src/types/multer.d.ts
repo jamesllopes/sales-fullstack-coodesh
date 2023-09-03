155ad5a0e1b5d6c2e603c9908dff64d462d843b7
James Lopes <jl.jameslopes@gmail.com>
2023-09-03 11:07:39 -0300

    feat: create controller uipload files

--- sales_api/src/types/multer.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "multer" {
-  import { RequestHandler } from "express";
-
-  interface File extends Express.Multer.File {} // Isso estende o tipo File do multer para evitar erros de tipo
-
-  function multer(options?: any): RequestHandler;
-
-  export = multer;
-}
